5805b9af34c681f56f9216136dea44db67c09652
Blanca Ley <blanca.ley@outlook.com>
2022-05-17 17:38:23 +0200

    Use middleware for parsing JSON

--- server.js
+++ server.js
@@ -3,6 +3,9 @@ import express from "express";
 const port = 3000;
 const app = express();
 
+// Middlewares
+app.use(express.json());
+
 app.listen(port, () => {
   console.log(`Listening on port ${port}`)
 })
