29e79e8dbf4723bc46a5924ef51e5a28744cdbeb
Blanca Ley <blanca.ley@outlook.com>
2022-05-18 12:18:24 +0200

    Import session middleware for authentication and cookies

--- server.js
+++ server.js
@@ -1,5 +1,6 @@
 import express from "express";
 import { MongoClient } from "mongodb";
+import session from "express-session";
 
 const port = 3000;
 const app = express();
@@ -14,6 +15,15 @@ const accountsCollection = db.collection("accounts");
 // Middlewares
 app.use(express.json());
 app.use(express.static("public"));
+app.use(session({
+  resave: false, // don't save session if unmodified
+  saveUninitialized: false, // don't create session until something stored
+  secret: 'shhhh, very secret',
+  cookie: {
+    maxAge: 5 * 60 * 1000 // 5 minutes
+  }
+ }));
+
 
 // Authentication
 // Route to create a new user
@@ -25,6 +35,9 @@ app.post("/api/register", async (req, res) => {
     username: req.body.username
   })
 })
+
+// Bank account logic
+// Route to create a new account
 
 app.listen(port, () => {
   console.log(`Listening on port ${port}`)
