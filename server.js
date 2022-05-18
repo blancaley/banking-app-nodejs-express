69d2299e84b48bc2d816ead7139df50fce762ca7
Blanca Ley <blanca.ley@outlook.com>
2022-05-18 11:38:04 +0200

    Connect to database and create users and accounts collection

--- server.js
+++ server.js
@@ -3,6 +3,13 @@ import express from "express";
 const port = 3000;
 const app = express();
 
+// Connect to database
+const client = new MongoClient("mongodb://127.0.0.1:27017");
+await client.connect();
+const db = client.db("bankingapp");
+const usersCollection = db.collection("users");
+const accountsCollection = db.collection("accounts");
+
 // Middlewares
 app.use(express.json());
 app.use(express.static("public"));
