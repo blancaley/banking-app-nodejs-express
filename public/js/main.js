ff4219669c3309637b88cfed852b74dcc1550932
Blanca Ley <blanca.ley@outlook.com>
2022-05-20 16:07:34 +0200

    Reload page after login, register or logout to trigger logged in status check

--- public/js/main.js
+++ public/js/main.js
@@ -7,16 +7,19 @@ import { isLoggedIn } from "./modules/isloggedin.mjs"
 loginForm.addEventListener("submit", async (e) => {
   e.preventDefault();
   await logIn();
+  location.reload();
 });
 
 registerForm.addEventListener("submit", async (e) => {
   e.preventDefault();
   await register();
+  location.reload();
 });
 
 logoutForm.addEventListener("submit", async (e) => {
   e.preventDefault();
   await logOut();
+  location.reload();
 });
 
 // Check if user is logged in every time page reloads
