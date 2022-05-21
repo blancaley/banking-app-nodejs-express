17aeb0830ce2ae5ae1d8231ccfceae21513dede7
Blanca Ley <blanca.ley@outlook.com>
2022-05-21 10:27:15 +0200

    Set up userPage renders for logged in user

--- public/js/modules/constants.mjs
+++ public/js/modules/constants.mjs
@@ -1,6 +1,13 @@
 const loginForm = document.getElementById("login");
 const registerForm = document.getElementById("register");
 const logoutForm = document.getElementById("logout");
+const userPage = document.getElementById("userPage");
 const welcomeMsg = document.getElementById("welcomeMsg");
 
-export { loginForm, registerForm, logoutForm, welcomeMsg }
+export { 
+  loginForm, 
+  registerForm, 
+  logoutForm, 
+  userPage, 
+  welcomeMsg 
+}
--- public/js/modules/render.mjs
+++ public/js/modules/render.mjs
@@ -1,15 +1,40 @@
-import { loginForm, registerForm, logoutForm, welcomeMsg } from "./constants.mjs"
+import { loginForm, registerForm, logoutForm, userPage, welcomeMsg } from "./constants.mjs"
+
+const userProfileWrapper = document.getElementById("userProfile");
+const createAccountWrapper = document.getElementById("createAccount");
+const userAccountsWrapper = document.getElementById("userAccounts");
+
+const renderUserProfile = () => {
+  userProfileWrapper.innerHTML =
+  `Test user profile`
+}
+
+const renderCreateAccounts = () => {
+  createAccountWrapper.innerHTML =
+  `Test account form`
+}
+
+const renderAccounts = () => {
+  userAccountsWrapper.innerHTML =
+  `Test account list`
+}
 
 const renderLoggedInPage = (user) => {
-    welcomeMsg.innerText = `Welcome ${user.firstName}`
-    logoutForm.hidden = false;
     loginForm.hidden = true;
     registerForm.hidden = true;
+    userPage.hidden = false;
+    welcomeMsg.innerText = `Welcome ${user.firstName}`
+    logoutForm.hidden = false;
+
+    renderUserProfile();
+    renderCreateAccounts();
+    renderAccounts();
 };
 
 const renderPublicPage = () => {
     // Hide logout button if user is not logged in
     logoutForm.hidden = true;
+    userPage.hidden = true;
 }
 
 export { renderLoggedInPage, renderPublicPage }
