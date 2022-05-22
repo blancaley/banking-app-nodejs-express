1d6e3bc2dd9160cd88024baf4d4881124e536211
Blanca Ley <blanca.ley@outlook.com>
2022-05-22 09:38:59 +0200

    Create new account from UI

--- public/js/modules/account/createAccount.mjs
+++ public/js/modules/account/createAccount.mjs
@@ -1,10 +1,13 @@
+import { isLoggedIn } from "../authentication/isloggedin.mjs";
+
 const createAccount = async () => {
   const accountName = document.getElementById("accountName");
   const amount = document.getElementById("amount");
 
-  // Get logged in user ID
+  // Get user info from cookie session
+  const user = await isLoggedIn();
   
-  const res = await fetch("/api/users/62879dfdd9cf9ad203bd8660/accounts", {
+  const res = await fetch(`/api/users/${user.userID}/accounts`, {
     method: "POST",
     headers: {
       "Content-Type": "application/json"
